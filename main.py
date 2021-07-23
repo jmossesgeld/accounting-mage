1889a0a75dbcdb5c134a180eecd92af592aefb88
Mira Jana <miragefallingsky@gmail.com>
2021-07-23 09:38:28 +0800

    test upload functionality

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
+from flask import send_from_directory
 import os
 from flask import Flask, flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = '/uploads'
+UPLOAD_FOLDER = '/path/to/the/uploads'
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
@@ -20,16 +23,16 @@ def upload_file():
             flash('No file part')
             return redirect(request.url)
         file = request.files['file']
-        # If the user does not select a file, the browser submits an
-        # empty file without a filename.
+        # if user does not select file, browser also
+        # submit an empty part without filename
         if file.filename == '':
             flash('No selected file')
             return redirect(request.url)
-        if file:
+        if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            print(file.content_type)
-            file.save(f"{app.config['UPLOAD_FOLDER']}/{filename}")
-            return redirect(url_for('download_file', name=filename))
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
     return '''
     <!doctype html>
     <title>Upload new File</title>
@@ -40,7 +43,6 @@ def upload_file():
     </form>
     '''
 
-from flask import send_from_directory
 
 @app.route('/uploads/<name>')
 def download_file(name):
