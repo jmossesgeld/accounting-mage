4d6d9e1925a2a8b88cfd4cb05c58eedec9d19217
Mira Jana <miragefallingsky@gmail.com>
2021-08-01 10:00:17 +0800

    add qap-convert

--- excel_convert.py
+++ excel_convert.py
@@ -6,7 +6,8 @@ import os
 THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
 dat_file = os.path.join(THIS_FOLDER, 'result.DAT')
 
-def convert(file):
+
+def slsp(file):
     # LOAD EXCEL FILE
     wb = load_workbook(file, data_only=True)
     ws = wb.worksheets[0]
@@ -22,7 +23,99 @@ def convert(file):
 
     try:
         ws['B2'].number_format = "m/dd/yyyy"
-        PERIOD = datetime.datetime.strftime(ws['B2'].value,"%m/%d/%Y")
+        PERIOD = datetime.datetime.strftime(ws['B2'].value, "%m/%d/%Y")
+    except (ValueError, TypeError):
+        has_error = True
+        error_msgs += 'Please fill-up PERIOD<br>'
+
+    CALENDAR = ws['B3'].value
+
+    try:
+        TIN = f"{ws['B7'].value.replace('-','')}"
+    except AttributeError:
+        try:
+            TIN = f"{ws['B7'].value:0>9d}"
+        except Exception:
+            has_error = True
+            error_msgs += 'Please fill-up Filer TIN<br>'
+
+    NAME = ws['B8'].value
+    TRADE = ws['B12'].value
+    ADDRESS1 = ws['B13'].value
+    ADDRESS2 = ws['B14'].value
+
+    # GET FILER TOTALS
+    EXEMPT = f"{ws['B18'].value:.2f}"
+    ZERO_RATED = f"{ws['B19'].value:.2f}"
+    SERVICES = f"{ws['B20'].value:.2f}"
+    CAPITAL_GOODS = f"{ws['B21'].value:.2f}"
+    GOODS = f"{ws['B22'].value:.2f}"
+    INPUT_VAT = f"{ws['B23'].value:.2f}"
+    CREDITABLE = f"{ws['B24'].value:.2f}"
+    NON_CREDITABLE = f"{ws['B25'].value:.2f}"
+    NO_OF_RECORDS = f"{ws['B26'].value:.2f}"
+
+    # READ AND FIX DATA
+    df = pd.read_excel(file, 'DATA').fillna(0)
+    df['ADDRESS 1'] = df['ADDRESS 1'].replace(0, '-')
+    df['ADDRESS 2'] = df['ADDRESS 2'].replace(0, '-')
+    df['LAST NAME'] = df['LAST NAME'].replace(0, '')
+    df['FIRST NAME'] = df['FIRST NAME'].replace(0, '')
+    df['MIDDLE NAME'] = df['MIDDLE NAME'].replace(0, '')
+
+    # CONVERT AND FORMAT DATA
+    def parse(line):
+        try:
+            line[0] = f"{line[0].replace('-','')}"
+        except AttributeError:
+            line[0] = f"{line[0]:0>9d}"
+            print(line[0])
+            print(type(line[0]))
+
+        line[7] = f'{line[7]:.2f}'
+        line[8] = f'{line[8]:.2f}'
+        line[9] = f'{line[9]:.2f}'
+        line[10] = f'{line[10]:.2f}'
+        line[11] = f'{line[11]:.2f}'
+        line[12] = f'{line[12]:.2f}'
+
+        for i in range(len(line)):
+            try:
+                line[i] = line[i].upper()
+            except AttributeError:
+                pass
+
+        return f'D,P,"{line[0]}","{line[1]}",,,,"{line[5]}","{line[6]}",{line[7]},{line[8]},{line[9]},{line[10]},{line[11]},{line[12]},{TIN},{PERIOD}\n'
+
+    # RETURN RESULT
+    if has_error:
+        return error_msgs
+    else:
+        with open(dat_file, 'w') as dat:
+            dat.write(f'H,P,"{TIN}","{NAME}","","","","{TRADE}","{ADDRESS1}","{ADDRESS2}",{EXEMPT},{ZERO_RATED},{SERVICES},{CAPITAL_GOODS},{GOODS},{INPUT_VAT},{CREDITABLE},{NON_CREDITABLE},{RDO_CODE},{PERIOD},{CALENDAR}\n')
+            dat.writelines([parse(line) for line in df.values])
+
+        dest_path = f'{TIN}P{PERIOD[:2]}{PERIOD[6:10]}.DAT'
+        return dest_path
+
+
+def qap(file):
+    # LOAD EXCEL FILE
+    wb = load_workbook(file, data_only=True)
+    ws = wb.worksheets[0]
+    error_msgs = '<strong>Errors:</strong><br>'
+    has_error = False
+
+    # GET FILER DATA
+    try:
+        RDO_CODE = f"{ws['B1'].value:0>3d}"
+    except Exception:
+        has_error = True
+        error_msgs += 'Please fill-up RDO Code<br>'
+
+    try:
+        ws['B2'].number_format = "m/dd/yyyy"
+        PERIOD = datetime.datetime.strftime(ws['B2'].value, "%m/%d/%Y")
     except ValueError:
         has_error = True
         error_msgs += 'Please fill-up PERIOD<br>'
@@ -86,7 +179,7 @@ def convert(file):
 
         return f'D,P,"{line[0]}","{line[1]}",,,,"{line[5]}","{line[6]}",{line[7]},{line[8]},{line[9]},{line[10]},{line[11]},{line[12]},{TIN},{PERIOD}\n'
 
-    #RETURN RESULT
+    # RETURN RESULT
     if has_error:
         return error_msgs
     else:
--- main.py
+++ main.py
@@ -1,30 +1,45 @@
 from flask import Flask, request, redirect, url_for, render_template, send_file
-from excel_convert import convert
-
+import excel_convert
 
 app = Flask(__name__)
 
 
-@app.route('/tools', methods=['GET', 'POST'])
-def tools():
+@app.route('/slsp-convert', methods=['GET', 'POST'])
+def slsp_convert():
+    path = ""
     if request.method == 'POST':
         file = request.files['file']
-        path = convert(file)
-        if path[:5] == '<stro':
-            return render_template('tools.html',error=path)
-        else:
+        path = excel_convert.slsp(file)
+        if path[:5] != '<stro':
             return send_file('result.DAT', as_attachment=True, attachment_filename=path)
+    return render_template('excel-convert.html', BIR_form='2550Q SLSP', version='RELIEF version: 2.3', template='PURCHASES_TEMPLATE.xlsm', error=path)
+
+
+@app.route('/qap-convert', methods=['GET', 'POST'])
+def qap_convert():
+    path = ""
+    if request.method == 'POST':
+        file = request.files['file']
+        path = excel_convert.qap(file)
+        if path[:5] != '<stro':
+            return send_file('result.DAT', as_attachment=True, attachment_filename=path)
+    return render_template('excel-convert.html', BIR_form='1601EQ QAP', version='Alphalist version: 7.0', template='QAP_TEMPLATE.xlsm', error=path)
+
+
+@app.route('/download-template/<path>')
+def download_template(path):
+    return send_file(path, as_attachment=True, attachment_filename=path)
+
+
+@app.route('/tools', methods=['GET', 'POST'])
+def tools():
     return render_template('tools.html')
 
-
-@app.route('/slsp/purchases/template')
-def purchase_template():
-    path = 'PURCHASES_TEMPLATE.xlsm'
-    return send_file(path, download_name=path)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
