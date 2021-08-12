cf6a0702eb592850fab5398def8a9a0baa036885
Mira Jana <miragefallingsky@gmail.com>
2021-08-12 20:01:16 +0800

    improve email encoding

--- email_forwarder.py
+++ email_forwarder.py
@@ -2,7 +2,7 @@ import imaplib
 import email
 import csv
 import smtplib
-import os 
+import os
 
 accounts_path = f"{os.path.dirname(os.path.realpath(__file__))}/email_accounts.csv"
 
@@ -33,39 +33,41 @@ for index, item in enumerate(account_list):
         # print("Mail IDs : {}\n".format(mails[0].decode().split()))
 
         ############### Select unread email IDs #############
-        mail_list = mails[0].decode().split()
+        mail_list = mails[0].decode('utf-8').split()
         max_id = len(mail_list)
         last_id = int(prev_id)
         max_count = min(max_id - last_id, 5)
 
         if max_count <= 0:
-            print("All emails updated, no email to forward.")
+            print(f"All emails updated, no email to forward. {max_id} - {last_id}")
             print("\nClosing selected mailbox....")
             imap_ssl.close()
             continue
         else:
             unread_mails = mail_list[-max_count:]
 
-
         ############### Read each email #############
         for mail_id in unread_mails:
             print(f"================== Start of Mail [{mail_id}] ====================")
             resp_code, mail_data = imap_ssl.fetch(mail_id, '(RFC822)')  # Fetch mail data.
             message = email.message_from_bytes(mail_data[0][1])  # Construct Message from mail data
-            _from = f"From\t\t\t: {message.get('From')}"
-            _date = f"Date\t\t\t: {message.get('Date')}"
-            _subject = f"Subject\t\t\t: {message.get('Subject')}"
-            _body = f"Message : \n\n{message.get_payload()}"
-            full_message = f"{_from}\n{_date}\n{_subject}\n{_body}"
-            print(full_message)
+            print(message.get('From'))
+            print(message.get('To'))
+            print(message.get('Date'))
+            print(message.get('Subject'))
+            print(message.get_payload())
             print(f"================== End of Mail [{mail_id}] ====================\n")
             with smtplib.SMTP("smtp.gmail.com") as connection:
                 connection.starttls()
                 connection.login("mage.automailer@gmail.com", "Magical2019")
+                message.replace_header('Subject', f"Forwarded from: {message.get('From')}")
+                message.replace_header('From', message.get('To'))
+                print(f"New Subject: {message.get('Subject')}, New sender: {message.get('From')}")
                 connection.sendmail(
-                    from_addr=message.get('From'),
+                    from_addr="mage.automailer@gmail.com",
                     to_addrs=forwardee,
-                    msg=f'Subject: Forwarded from: {email_id}\n\n{full_message}')
+                    msg=message.as_bytes())
+                print('Message Sent!')
 
         ############# Close Selected Mailbox #######################
         print("\nClosing selected mailbox....")
